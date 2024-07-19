b8da7c853db10d554ae4b8dae1bb8b33b318a455
ernandiyabadriatulkhusniyah <ernandiyabadriatulkhusniyah@gmail.com>
2024-07-19 21:59:42 +0700

    second change

---
+++ SkinScan/lib/themes/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  static Color kPrimary = const Color(0XFF1460F2);
+  static Color kWhite = const Color(0XFFFFFFFF);
+  static Color kBackground = const Color(0XFFFAFAFA);
+  static Color kGrayscaleDark100 = const Color(0XFF1C1C1E);
+  static Color kLine = const Color(0XFFEBEBEB);
+  static Color kBackground2 = const Color(0XFFF6F6F6);
+  static Color kGrayscale40 = const Color(0XFFAEAEB2);
+  static Color bgColor =  Color.fromARGB(255, 39, 54, 127);
+  static Color bgColor1 =  Color.fromARGB(255, 91, 109, 191);
+  static Color button = Color.fromARGB(255, 44, 88, 158);
+}
